868ec149b33bf72e8e03b55c86d6512a40356bc9
123Somrat <jafaruddin875@gmail.com>
2024-08-12 13:10:52 +0200

    setup route

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
 
-
-app.get("/health", (_req, res) => {
-  res.json({
-    health: "OK",
-    message: "Boddha biaggin tik tak,Tui ham gor boddha",
-    code: 200,
-  });
-});
+app.get('/api/v1/health',(_req,res)=>{
+   res.json({
+       health:"OK",
+       message:'Boddha biaggin tik tak,tui ham gor',
+       code : 200
+   })
+})
 
 
 
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,11 +1,13 @@
 import app from "../app"
 import cors from "cors";
 import express, { Application, json } from "express";
+import router from "../routes";
 
 
 const middleWare = (app:Application)=>{
  app.use(cors())
  app.use(express.json())
+ app.use(router)
 
 }
 
