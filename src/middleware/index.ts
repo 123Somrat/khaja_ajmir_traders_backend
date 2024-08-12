3a917e06195a63bb1027acd97067729189f595db
123Somrat <jafaruddin875@gmail.com>
2024-08-12 13:18:07 +0200

    setup global error middleWare

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,19 +1,21 @@
-import app from "../app"
+import app from "../app";
 import cors from "cors";
-import express, { Application, json } from "express";
+import express, { Application, Request, Response, NextFunction } from "express";
 import router from "../routes";
 
+const middleWare = (app: Application) => {
+  app.use(cors());
+  app.use(express.json());
+  app.use(router);
+};
 
-const middleWare = (app:Application)=>{
- app.use(cors())
- app.use(express.json())
- app.use(router)
-
-}
-
-
-
-
-
+app.use((err:any, req: Request, res: Response, next: NextFunction) => {
+  next();
+  res.status(err.status).json({
+    status: err.status || 500,
+    code: err.code || "Internal server error",
+    message: err.message || "Opps something wrong in our site",
+  });
+});
 
 export default middleWare;
