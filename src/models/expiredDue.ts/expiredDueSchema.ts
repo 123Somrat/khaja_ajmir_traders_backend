0e78e1c8260704c2c4799cc6ae5809bae18ab602
123Somrat <jafaruddin875@gmail.com>
2024-08-27 10:34:12 +0200

    Create expired due schema

---
+++ src/models/expiredDue.ts/expiredDueSchema.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+import dueType from "../due/dueTypes";
+
+const expiredDueSchema = new Schema<dueType>({
+  buyerName: {
+    type: String,
+    required: true,
+  },
+  sellerName: {
+    type: String,
+    required: true,
+  },
+  buyingPrice: {
+    type: Number,
+    required: true,
+  },
+  buyingDate: {
+    type: String,
+    required: true,
+  },
+  expiredDate: {
+    type: String,
+    required: true,
+  },
+});
+
+const expiredDueSchemaa = model("expiredDueSchema", expiredDueSchema);
+
+export default expiredDueSchemaa;
