5c4cf75ca947ef0578d33afac6ffe4ee60da37fb
123Somrat <jafaruddin875@gmail.com>
2024-08-28 11:57:20 +0200

    Useing seassion and transection because have to do mutiple read write

--- src/lib/cronJobs/index.ts
+++ src/lib/cronJobs/index.ts
@@ -1,10 +1,10 @@
 import cron from "node-cron";
 import dueModel from "../../models/due/dueSchema";
 import dayjs from "dayjs";
-import expiredDueService from '../expiredDue'
-import dueService from '../due'
-
-
+import expiredDueService from "../expiredDue";
+import dueService from "../due";
+import mongoose from "mongoose";
+import HttpError from "../../utils/customError";
 
 /**
  * * Run after every 6 hours
@@ -17,23 +17,38 @@ cron.schedule(
   async () => {
     console.log("Cron jobs running");
 
+    // Create a seassion
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
     try {
       const today = dayjs();
       // retrive the expired due from db
-      const expiredDue = await dueModel.find({ ecpiredDate: { $lt: today } });
+      const expiredDue = await dueModel
+        .find({ ecpiredDate: { $lt: today } })
+        .session(session);
       // Create a id array to which due have to delete
       const haveToDeleteDueFromDueModle = expiredDue.map((due) => due._id);
 
       // Insert and delete due
       if (expiredDue.length > 0) {
         // Called expiredDue service
-        const insertedExpiredDue = await expiredDueService.expiredDues(expiredDue)
-  
-         // call the deleteDueService
-        const deletedExpiredDueFromDueModel = await dueService.deleteDue(haveToDeleteDueFromDueModle)
+        const insertedExpiredDue = await expiredDueService.expiredDues(
+          expiredDue
+        );
+
+        // call the deleteDueService
+        const deletedExpiredDueFromDueModel = await dueService.deleteDue(
+          haveToDeleteDueFromDueModle
+        );
       }
-    } catch (error) {
-      console.error("Error executing cron job:", error);
+      // Commit transaction
+      await session.commitTransaction();
+      session.endSession();
+    } catch (err: any) {
+      await session.abortTransaction();
+      session.endSession();
+      throw new HttpError(err.status, err.code, err.message);
     }
   },
   {
