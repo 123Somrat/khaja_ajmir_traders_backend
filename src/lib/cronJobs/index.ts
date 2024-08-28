83effc312fde1098970e081b951bcdf32c1b7e65
123Somrat <jafaruddin875@gmail.com>
2024-08-28 11:35:55 +0200

    Automaically Insert expired due  after every 6 hours useing created crons jobs

--- src/lib/cronJobs/index.ts
+++ src/lib/cronJobs/index.ts
@@ -1,16 +1,31 @@
-import cron from 'node-cron';
-import dueModel from '../../models/due/dueSchema';
-import dayjs from 'dayjs';
-import expiredDueModel from '../../models/expiredDue.ts/expiredDueSchema';
+import cron from "node-cron";
+import dueModel from "../../models/due/dueSchema";
+import dayjs from "dayjs";
+import expiredDueModel from "../../models/expiredDue.ts/expiredDueSchema";
 
+cron.schedule(
+  "* */6 * * *",
+  async () => {
+    console.log("Cron jobs running");
 
+    try {
+      const today = dayjs();
+      // retrive the expired due from db
+      const expiredDue = await dueModel.find({ ecpiredDate: { $lt: today } });
+      // Create a id array to which due have to delete
+      const haveToDeleteDueFromDueModle = expiredDue.map((due) => due._id);
 
-cron.schedule('*/1 * * * *',async()=>{
-   console.log('Cron jobs running')
-
-},{
+      // Insert and delete due
+      if (expiredDue.length > 0) {
+        const insertedExpiredDue = await expiredDueModel.insertMany(expiredDue);
+       
+      }
+    } catch (error) {
+      console.error("Error executing cron job:", error);
+    }
+  },
+  {
     scheduled: true,
-    timezone: "Europe/Berlin"
-  })
-
-
+    timezone: "Europe/Berlin",
+  }
+);
