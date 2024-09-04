b4442ad7c7ddc4d0258aace91d66b0ee86790347
123Somrat <jafaruddin875@gmail.com>
2024-09-04 11:51:00 +0200

    Create soldOurDueSchema and model

---
+++ src/models/soldOutDue/soldOutDueSchema.ts
@@ -0,0 +1,36 @@
+import { Schema , model} from "mongoose";
+import dueType from "../../types/types";
+
+
+const soldOutDueSchema = new Schema<dueType>({
+     buyerName:{
+         type:String,
+         required:true
+     },
+     sellerName:{
+         type:String,
+         required:true
+     },
+     buyingPrice:{
+         type:Number,
+         required:true
+     },
+     sellingPrice:{
+        type:Number,
+        required:true
+     },
+     buyingDate:{
+         type :String,
+         required:true
+     },
+     expiredDate:{
+         type:String,
+         required:true
+     }
+
+})
+
+const soldOutDueSchemas = model<dueType>('soldOutDueSchema',soldOutDueSchema);
+
+
+export default soldOutDueSchema;
