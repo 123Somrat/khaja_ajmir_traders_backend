991b37d59eba92d107cf4995f7d807e248d97e4b
123Somrat <jafaruddin875@gmail.com>
2024-09-04 11:42:50 +0200

    Deleted expired due document after updated

--- src/lib/expiredDue/index.ts
+++ src/lib/expiredDue/index.ts
@@ -1,5 +1,5 @@
 import { Types } from "mongoose";
-import expiredDueModel from "../../models/expiredDue.ts/expiredDueSchema";
+import expiredDueModel from "../../models/expiredDue/expiredDueSchema";
 import dueType from "../../types/types";
 import HttpError from "../../utils/customError";
 import { ObjectId } from "mongodb";
@@ -9,13 +9,13 @@ import { ObjectId } from "mongodb";
  * @param expiredDues
  * @returns expiredDues
  */
-const expiredDues = async (expiredDues: dueType[] ) => {
+const expiredDues = async (expiredDues: dueType[]) => {
   try {
     const expiredDue = await expiredDueModel.create(expiredDues);
-     console.log('from expired due service',expiredDue)
+    console.log("from expired due service", expiredDue);
     return expiredDue;
   } catch (err: any) {
-    console.log('from expired dues error block',err)
+    console.log("from expired dues error block", err);
     if (err.code === 11000) {
       throw new HttpError(11000, "Dulicate key error", "item already exeist");
     }
@@ -44,32 +44,56 @@ const patchExpiredDues = async (paylode: {
   sellingPrice: number;
 }) => {
   try {
-    // Checking given id due exeist or not
-    const isExeist = await expiredDueModel.findById(paylode.id);
+    // Update expiredDue document
+    const updatedExpiredDue = await expiredDueModel.findOneAndUpdate(
+      { _id: new ObjectId(paylode.id) },
+      { $set: { sellingPrice: paylode.sellingPrice } },
+      {
+        returnDocument: "after", // Ensure this is set to "after" to get the updated document
+        returnOriginal: false, // For older versions of MongoDB, use this option
+        upsert: false, // Optional: set to true if you want to insert the document if it doesn't exist
+      }
+    );
 
-    if (!isExeist) {
+    // If due not found throw not found error
+    if (!updatedExpiredDue) {
       throw new HttpError(404, "Not found", "No data found");
     }
-   
-    // Update expiredDue document
-    const updatedExpiredDue =await expiredDueModel.updateOne({_id:new ObjectId(paylode.id)},{$set:{sellingPrice:paylode.sellingPrice}})
-    
-   
 
-    if (updatedExpiredDue.modifiedCount === 0) {
+    // Throw bad request error
+    if (!updatedExpiredDue) {
       throw new HttpError(400, "Bad Request", "Document not updated");
     }
 
-
-      //Todo have to dekete the document
-     if(updatedExpiredDue.modifiedCount===1){
-        return 'somrat'
-     }
-
+    // If due updated succesfully then delete iot from expired due
+    if (updatedExpiredDue) {
+      const deletedSoldDue = await deleteAexpiredDue(paylode.id);
+    }
   } catch (err) {
     if (err instanceof HttpError) {
       throw new HttpError(err.status, err.code, err.message);
     }
+  }
+};
+
+/**
+ *
+ * @param id
+ * !return deleted due
+ */
+const deleteAexpiredDue = async (id: string) => {
+  try {
+    const isExiest = expiredDueModel.findById(id);
+
+    if (!isExiest) {
+      throw new HttpError(404, "Not found", "No data found");
+    }
+
+    const deletedSoldDue = expiredDueModel.findByIdAndDelete(id);
+
+    return deletedSoldDue;
+  } catch (err) {
+    console.log(err);
   }
 };
 
