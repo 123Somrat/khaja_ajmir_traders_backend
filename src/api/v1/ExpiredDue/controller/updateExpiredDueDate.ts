c419b35e1cf58210a69e3b3c90e279c7cfe50e80
123Somrat <jafaruddin875@gmail.com>
2024-09-16 14:08:53 +0200

    Crate dueUpdate service and after update send it as a response

--- src/api/v1/ExpiredDue/controller/updateExpiredDueDate.ts
+++ src/api/v1/ExpiredDue/controller/updateExpiredDueDate.ts
@@ -1,11 +1,28 @@
 import asyncHandeler from "../../../../utils/asyncHandeler";
+import expiredDueService from "../../../../lib/expiredDue";
+const updateExpiredDueDate = asyncHandeler(async (req, res, next) => {
+  // Gettign id and data from req
+  const id = req.params.id;
+  const updatedBuyingPrice = req.body.data.updatedBuyingPrice;
+  const updatedExpiredDueDate = req.body.data.updatedExpiredDate;
 
-const updateExpiredDueDate = asyncHandeler(async(req,res,next)=>{
-       console.log('i am from updateExpiredDueDate',req.body.data)
+ 
+
+  // Call updateExpiredDue service for update expiredDate
+  const updatedDue = await expiredDueService.updateExpiredDueDates({
+    id,
+    updatedBuyingPrice,
+    updatedExpiredDueDate,
+  });
 
 
+  // Send updated due as a response
+  res.status(200).json({
+    status: 200,
+    code: "Ok",
+    message: "Due updated succesfully",
+    data: updatedDue,
+  });
+});
 
-
-})
-
-export default updateExpiredDueDate
+export default updateExpiredDueDate;
--- src/lib/expiredDue/index.ts
+++ src/lib/expiredDue/index.ts
@@ -4,8 +4,7 @@ import dueType from "../../types/types";
 import HttpError from "../../utils/customError";
 import { ObjectId } from "mongodb";
 import soldOutDueService from "../soldOutDue";
-
-
+import dueService from "../due";
 /**
  *
  * @param expiredDues
@@ -14,7 +13,7 @@ import soldOutDueService from "../soldOutDue";
 const expiredDues = async (expiredDues: dueType[]) => {
   try {
     const expiredDue = await expiredDueModel.create(expiredDues);
-    
+
     return expiredDue;
   } catch (err: any) {
     console.log("from expired dues error block", err);
@@ -44,7 +43,70 @@ const getAllExpiredDues = async () => {
 
 /**
  * 
- * @param paylode 
+ * @param id updatedBuyingPrice updatedExpiredDueDate
+ * @returns newCreatedDue
+ */
+
+const updateExpiredDueDates = async ({
+  id,
+  updatedBuyingPrice,
+  updatedExpiredDueDate,
+}: {
+  id: string;
+  updatedBuyingPrice: string;
+  updatedExpiredDueDate: string;
+}) => {
+  try {
+
+     // Checking data exiest or not
+    const isDueExeist = await expiredDueModel.findById(id);
+
+ 
+    // Throwing error
+    if (!isDueExeist) {
+      console.log('inside not fund')
+      throw new HttpError(404, "Not Found", "Not exeist");
+    }
+   
+
+    // formatted newDue object
+    const newDue = isDueExeist && {
+      buyerName: isDueExeist?.buyerName,
+      sellerName: isDueExeist?.sellerName,
+      buyingPrice: Number(updatedBuyingPrice),
+      buyingDate: isDueExeist?.buyingDate,
+      expiredDate: updatedExpiredDueDate,
+      sellingPrice: 0,
+    };
+
+    // Delete oldDueFromExpiredDueModal afetr insert in dueModel
+    const newCreatedDue = await dueService.createDue(newDue);
+
+    if (newCreatedDue) {
+      const deletedOldDue = await expiredDueModel.findByIdAndDelete(id);
+    }
+
+    return newCreatedDue;
+
+    
+  } catch (err) {
+
+    if (err instanceof HttpError) {
+      throw new HttpError(err.status, err.code, err.message);
+    } else {
+   
+      throw new HttpError(
+        500,
+        "Internal server error",
+        "An unexpected error occurred"
+      );
+    }
+  }
+};
+
+/**
+ *
+ * @param paylode
  * * Updated the expired due selling price
  * * After updated succesfully then insert the expired due into SoldOut due
  * * After succesfully insert the due into sold out due then delete it from expired dues
@@ -52,7 +114,7 @@ const getAllExpiredDues = async () => {
 const patchExpiredDues = async (paylode: {
   id: string;
   sellingPrice: number;
-  sellingDate:string
+  sellingDate: string;
 }) => {
   try {
     // Update expiredDue document
@@ -66,9 +128,6 @@ const patchExpiredDues = async (paylode: {
       }
     );
 
- 
-
-
     // If due not found throw not found error
     if (!updatedExpiredDue) {
       throw new HttpError(404, "Not found", "No data found");
@@ -80,23 +139,22 @@ const patchExpiredDues = async (paylode: {
     }
     // If due updated succesfully then insert The due into soldOur due scchema
     if (updatedExpiredDue) {
-      const soldOutDue =await soldOutDueService.soldOutDues({buyerName: updatedExpiredDue.buyerName, 
+      const soldOutDue = await soldOutDueService.soldOutDues({
+        buyerName: updatedExpiredDue.buyerName,
         sellerName: updatedExpiredDue.sellerName,
         buyingPrice: updatedExpiredDue.buyingPrice,
         sellingPrice: updatedExpiredDue.sellingPrice,
-        buyingDate:updatedExpiredDue.buyingDate,
-        expiredDate:updatedExpiredDue.expiredDate,
-        sellingDate:paylode.sellingDate
-      })
+        buyingDate: updatedExpiredDue.buyingDate,
+        expiredDate: updatedExpiredDue.expiredDate,
+        sellingDate: paylode.sellingDate,
+      });
 
-     
       //After insert the due into soldOut due schema then Deleted dues from expired dues
-      if(soldOutDue){
-         const deletedSoldDue = await deleteAexpiredDue(paylode.id);
-      
-          return deletedSoldDue
+      if (soldOutDue) {
+        const deletedSoldDue = await deleteAexpiredDue(paylode.id);
+
+        return deletedSoldDue;
       }
-    
     }
   } catch (err) {
     if (err instanceof HttpError) {
@@ -117,13 +175,18 @@ const deleteAexpiredDue = async (id: string) => {
     if (!isExiest) {
       throw new HttpError(404, "Not found", "No data found");
     }
-     
+
     const deletedSoldDue = expiredDueModel.findByIdAndDelete(id);
-      
+
     return deletedSoldDue;
   } catch (err) {
     console.log(err);
   }
 };
 
-export = { expiredDues, getAllExpiredDues, patchExpiredDues };
+export = {
+  expiredDues,
+  getAllExpiredDues,
+  updateExpiredDueDates,
+  patchExpiredDues,
+};
