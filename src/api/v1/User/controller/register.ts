ea2c22468ccac5149972bba79d1892e3931672a5
123Somrat <jafaruddin875@gmail.com>
2024-09-24 10:49:54 +0200

    Transformed cretaed user data to send in response

--- src/api/v1/User/controller/register.ts
+++ src/api/v1/User/controller/register.ts
@@ -1,21 +1,25 @@
 import asyncHandeler from "../../../../utils/asyncHandeler";
-import authService from '../../../../lib/auth'
-const register = asyncHandeler(async(req,res,next)=>{
+import authService from "../../../../lib/auth";
+import TUser from "../../../../models/user/userType";
+const register = asyncHandeler(async (req, res, next) => {
+  // getting UserInfo from req
+  const userInfo = req.body.data;
 
-   // getting UserInfo from req
-   const userInfo = req.body.data;
- 
- // Call userRegistretion service for create user
- const user = await authService.register(userInfo)
+  // Call userRegistretion service for create user
+  const user = await authService.register(userInfo);
+  const { name, email } = user as TUser;
+  // transform user object
+  const transFormedUserObject = {
+    name,
+    email,
+  };
 
- 
   res.status(201).json({
-      status:201,
-      code:'Created succesfully',
-      messege:'User created successfullly',
-      data:user
-  })
+    status: 201,
+    code: "Created succesfully",
+    messege: "User created successfullly",
+    data: transFormedUserObject,
+  });
+});
 
-})
-
-export default register
+export default register;
