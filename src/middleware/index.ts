c5f420f81caa10163fa7df2985766bdf364b40e4
123Somrat <jafaruddin875@gmail.com>
2024-10-07 11:04:11 +0200

    package(compression) : install compression for compress the response size

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import express, { Application, Request, Response, NextFunction } from "express";
 import router from "../routes";
 import HttpError from "../utils/customError";
-
+import compression from 'compression'
 // CORS options
 const corsOptions = {
   origin: "http://localhost:5173",
@@ -16,6 +16,8 @@ const middleWare = (app: Application) => {
   app.use(cors(corsOptions));
   app.use(express.json());
   app.use(router);
+  // compress all responses
+  app.use(compression())
 
   app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
     res.status(err.status).json({
