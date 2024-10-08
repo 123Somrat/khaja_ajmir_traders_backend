63b6802a0d6ea2f0fa6a0035889a7bc435c745c0
123Somrat <jafaruddin875@gmail.com>
2024-10-08 10:48:09 +0200

    remove(unusual log) : remove inusual log

--- src/api/v1/Due/controller/allDues.ts
+++ src/api/v1/Due/controller/allDues.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from "express";
 import dueService from "../../../../lib/due";
 import query from "../../../../utils/query";
 
-
 const allDues = async (req: Request, res: Response, next: NextFunction) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 5;
@@ -12,7 +11,6 @@ const allDues = async (req: Request, res: Response, next: NextFunction) => {
   const searchBy = (req.query.searchBy as string) || "";
 
   // Call allDues service for getting all dues from db
-  console.time('response')
   const allDue = await dueService.allDues(
     page,
     limit,
@@ -21,20 +19,12 @@ const allDues = async (req: Request, res: Response, next: NextFunction) => {
     searchBy
   );
 
-  console.timeEnd('response')
-
-
-
-console.time('times')
   // Count total items depends on search for pagination
   const totalItems = await dueService.count(searchBy);
- console.timeEnd('times')
-  console.time('pagination')
+
   // get pagination data
   const pagination = query.getPagination({ page, limit, totalItems });
- console.timeEnd('pagination')
 
- console.time('hate')
   // getHateOs links
   const hateOsLinks = query.generateHateOsLinks({
     url: req.url,
@@ -43,7 +33,7 @@ console.time('times')
     hasNext: !!pagination.next,
     hasPrev: !!pagination.prev,
   });
- console.timeEnd('hate')
+
   // send response
   res.status(200).json({
     status: 200,
@@ -53,9 +43,6 @@ console.time('times')
     meta: pagination,
     hateOsLinks,
   });
-  console.log('response done')
-  
-
 };
 
 export default allDues;
