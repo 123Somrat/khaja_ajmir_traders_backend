93ccb42af292f4df586cae80e71eea26df6f3753
123Somrat <jafaruddin875@gmail.com>
2025-03-05 14:00:43 +0100

    test(userService) : improve coverage for userService

--- tests/unit/Auth/register.test.ts
+++ tests/unit/Auth/register.test.ts
@@ -1,5 +1,6 @@
 import userModel from "../../../src/models/user/userSchema";
 import userService from "../../../src/lib/user";
+import HttpError from "../../../src/utils/customError";
 
 
 describe("Register", () => {
@@ -24,4 +25,14 @@ describe("Register", () => {
         expect(user).toEqual(true);
         console.log('test passed')
     });
+
+    it("should throw an error if user creation fails", async () => {
+        userModel.create = jest.fn().mockRejectedValue(new Error("Database Error"));
+      
+        await expect(userService.createUser(mockData)).rejects.toThrow(new HttpError(500, "Internal server error", "An unexpected error occured"));
+      });
+
+
+
+
 });
